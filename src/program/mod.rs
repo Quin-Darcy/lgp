ff945a7405a3bc7b2d7cccf26efa9d3f11e30b74
Quin Darcy <pohmsuindraguli@gmail.com>
2025-01-30 14:25:18 -0600

    Update mod.rs

--- src/program/mod.rs
+++ src/program/mod.rs
@@ -1,26 +1,5 @@
-mod operator;
-mod instruction;
-
-pub use instruction::Instruction;
-pub use operator::Operator;
+pub mod operator;
+pub mod instruction;
 
 // Register configuration
 pub type RegisterIndex = u8;
-pub const TOTAL_REGISTERS: usize = 8;
-pub const INPUT_REGISTER: RegisterIndex = 1;
-pub const OUTPUT_REGISTER: RegisterIndex = 0;
-
-pub struct Program {
-    instructions: Vec<Instruction>,
-    pub registers: [f64; TOTAL_REGISTERS],
-}
-
-impl Program {
-    pub fn new() -> Self {
-        Self {
-            instructions: Vec::new(),
-            registers: [1.0; TOTAL_REGISTERS],
-        }
-    }
-}
-
