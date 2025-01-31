26ffbf8fd6795aef170e6a4e321aef9489f91b4e
Quin Darcy <pohmsuindraguli@gmail.com>
2025-01-31 11:53:01 -0600

    Update main.rs with test of program

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use lgp::program::Program;
+
+fn main() {
+    let initial_size: usize = 8;
+    let mut program = Program::new(initial_size);
+    println!("{:?}", program.run(0.0));
+}
