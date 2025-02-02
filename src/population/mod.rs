1c4b0c73847a5498e3e6f40d82bf4fe4c88cd053
Quin-Darcy <pohmsuindraguli@gmail.com>
2025-02-01 22:54:17 -0600

    Add new() method and start evolve method

--- src/population/mod.rs
+++ src/population/mod.rs
@@ -4,6 +4,7 @@
 //! which evolves the programs. An instance of a Population consists of the following fields:
 //!
 //! - `individuals`: This is a vector of Programs. 
+//! - `fitnesses`: Vector containing the fitness value for each individual.
 //!
 //! This module defines how individual programs in the genetic programming system
 //! are represented and executed. Each program consists of:
@@ -35,7 +36,8 @@ const PROGRAM_SIZE: usize = 20;
 
 /// Structure containing the population of Programs.
 pub struct Population {
-    individuals: Vec<Program>
+    /// Vector containing the population of Programs.
+    pub individuals: Vec<Program>
 }
 
 impl Population {
@@ -52,5 +54,34 @@ impl Population {
         Self {
             individuals: indvs
         }
+    }
+
+    /// Main genetic algorithm loop to evolve a program against the given training data.
+    ///
+    /// # Arguments
+    /// * `training_data`: Set of data points against which the population's fitness is measured.
+    pub fn evolve(training_data: &Vec<(f64, f64)>) -> Program {
+       /*
+        * The main loop will consist of the following steps:
+        * 1. Evaluate fitness of each individual.
+        * 2. Perform selection.
+        * 3. Probabilistically perform mutation and crossover.
+        * 4. Fill population with new individuals from last step.
+        * 5. Repeat until individual fitness high enough.
+        */
+
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_population_new() {
+        let pop_size: usize = 38;
+        let pop = Population::new(pop_size);
+        assert_eq!(pop.individuals.len(), pop_size);
     }
 }
