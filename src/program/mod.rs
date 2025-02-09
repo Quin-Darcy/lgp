201cf0c4d4ce7bea9975090b0f04bb1637c0f3f6
Quin-Darcy <pohmsuindraguli@gmail.com>
2025-02-09 11:21:45 -0600

    Fix up unit tests

--- src/program/mod.rs
+++ src/program/mod.rs
@@ -96,7 +96,7 @@ impl Program {
     /// Mark the effective instructions in given code
     ///
     /// # Arguments
-    /// - `code`: Full program to be reduced
+    /// - `code`: Full program to be marked
     #[allow(clippy::missing_panics_doc)]
     pub fn mark_introns(code: &mut [Instruction]) {
         let mut effective_regs: Vec<RegisterIndex> = vec![
@@ -118,7 +118,12 @@ impl Program {
     /// * `input`: Input value which the program will operate on.
     #[allow(clippy::missing_panics_doc)]
     pub fn run(&mut self, input: f64) -> f64 {
+        // Reset variable registers before each run
+        self.var_registers = [1.0; TOTAL_VAR_REGISTERS];
+
+        // Set the input register
         self.var_registers[INPUT_REGISTER] = input;
+
         for inst in &self.instructions {
             // Check the effective instruction flag
             if inst.0 & 0x8000_0000 == 0 {
@@ -134,8 +139,10 @@ impl Program {
                 };
             }
 
-                self.var_registers[inst.dst() as usize] = inst.operator()
-                    .execute(operands[0], operands[1]);
+            self.var_registers[inst.dst() as usize] = inst.operator()
+                .execute(operands[0], operands[1]);
+
+            println!("{inst} = {:?}", self.var_registers[inst.dst() as usize]);
         }
         self.var_registers[OUTPUT_REGISTER]
     }
@@ -164,15 +171,28 @@ mod tests {
     #[test]
     fn test_program_run() {
         let input: f64 = 3.0;
-        let inst1 = Instruction(0x00023901);
-        let inst2 = Instruction(0x02033C02);
-        let inst3 = Instruction(0x03040302);
-        let inst4 = Instruction(0x03053B3C);
-        let inst5 = Instruction(0x0100043D);
-        let inst6 = Instruction(0x01000005);
+        let inst1 = Instruction(0x00023901); // VR[2] = CR[49] + VR[1]
+        let inst2 = Instruction(0x02033C02); // VR[3] = CR[52] * VR[2]
+        let inst3 = Instruction(0x03040302); // VR[4] = VR[3] / VR[2]
+        let inst4 = Instruction(0x03053B3C); // VR[5] = CR[51] / CR[52]
+        let inst5 = Instruction(0x0100043D); // VR[0] = VR[4] - CR[53]
+        let inst6 = Instruction(0x01000005); // VR[0] = VR[0] - VR[5]
+        
+        /*
+         * VR[2] = -1.0 + 3.0 = 2.0
+         * VR[3] = 2.0 * 2.0  = 4.0
+         * VR[4] = 4.0 / 2.0  = 2.0
+         * VR[5] = 1.0 / 2.0  = 0.5
+         * VR[0] = 2.0 - 3.0  = -1.0
+         * VR[0] = -1.0 - 0.5 = -1.5
+         */
 
         let mut prog = Program::new(6);
-        let mut inst_vec: Vec<Instruction> = vec![inst1, inst2, inst3, inst4, inst5, inst6];
+        let mut inst_vec: Vec<Instruction> = vec![
+            inst1, inst2, 
+            inst3, inst4, 
+            inst5, inst6
+        ];
 
         Program::mark_introns(&mut inst_vec);
         prog.instructions = inst_vec;
@@ -181,24 +201,56 @@ mod tests {
     }
 
     #[test]
-    fn test_intron_removal() {
-        let input: f64 = 3.0;
-        let inst1 = Instruction(0x00023901);
-        let inst2 = Instruction(0x00060705);
-        let inst3 = Instruction(0x02033C02);
-        let inst4 = Instruction(0x03040302);
-        let inst5 = Instruction(0x03053B3C);
-        let inst6 = Instruction(0x0100043D);
-        let inst7 = Instruction(0x03070605);
-        let inst8 = Instruction(0x01000005);
+    fn test_intron_marking() {
+        let input: f64 = 2.0; 
+        let inst1 = Instruction(0x0002_0103); // VR[2] = VR[1] + VR[3]
+        let inst2 = Instruction(0x0204_0203); // VR[4] = VR[2] * VR[3] <-- Intron
+        let inst3 = Instruction(0x0001_0301); // VR[1] = VR[3] + VR[1]
+        let inst4 = Instruction(0x0205_0203); // VR[5] = VR[2] * VR[3] <-- Intron
+        let inst5 = Instruction(0x0000_0102); // VR[0] = VR[1] + VR[2]
+        
+        
+        // Create vector of all instructions
+        let mut instructions = vec![inst1, inst2, inst3, inst4, inst5];
+        Program::mark_introns(&mut instructions);
 
-        let mut prog = Program::new(8);
-        let mut inst_vec: Vec<Instruction> = vec![inst1, inst2, inst3, inst4, inst5, inst6, inst7, inst8];
+        // We must confirm the correct instructions were marked as introns
+        assert!(instructions[0].0 & 0x8000_0000 == 0x8000_0000);
+        assert!(instructions[1].0 & 0x8000_0000 == 0);
+        assert!(instructions[2].0 & 0x8000_0000 == 0x8000_0000);
+        assert!(instructions[3].0 & 0x8000_0000 == 0);
+        assert!(instructions[4].0 & 0x8000_0000 == 0x8000_0000);
+    }
 
-        Program::mark_introns(&mut inst_vec);
-        prog.instructions = inst_vec;
+    #[test]
+    fn test_effective_code_run() {
+        let input: f64 = 2.0; 
+        let inst1 = Instruction(0x0002_0103); // VR[2] = VR[1] + VR[3]
+        let inst2 = Instruction(0x0204_0203); // VR[4] = VR[2] * VR[3]
+        let inst3 = Instruction(0x0001_0301); // VR[1] = VR[3] + VR[1]
+        let inst4 = Instruction(0x0205_0203); // VR[5] = VR[2] * VR[3]
+        let inst5 = Instruction(0x0000_0102); // VR[0] = VR[1] + VR[2]
+        
+        /*
+         * VR[2] = 2.0 + 1.0 = 3.0
+         * VR[4] = 3.0 * 1.0 = 3.0
+         * VR[1] = 1.0 + 2.0 = 3.0
+         * VR[5] = 3.0 * 1.0 = 3.0
+         * VR[0] = 3.0 + 3.0 = 6.0
+         */
 
-        assert_eq!(prog.run(input), -1.5);
+        // Create the program
+        let mut prog = Program::new(5);
+        
+        // Create vector of all instructions
+        let mut instructions = vec![inst1, inst2, inst3, inst4, inst5];
+
+        // Mark introns and update program's code
+        Program::mark_introns(&mut instructions);
+        prog.instructions = instructions;
+
+        // Confirm the output equals the correct output
+        assert_eq!(prog.run(input), 6.0);
     }
 }
 
