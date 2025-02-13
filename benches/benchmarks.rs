8bd085bbaee66fd1ee4e6e217a172982926ef1e9
Quin Darcy <pohmsuindraguli@gmail.com>
2025-02-13 10:12:13 -0600

    Add const RegisterConfig

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,12 +1,22 @@
 #![allow(dead_code)]
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use lgp::program::Program;
+use lgp::program::{Program, RegisterConfig};
+
+const CONFIG: RegisterConfig = RegisterConfig {
+    total_var_registers: 8,
+    total_const_registers: 100,
+    const_start: -50.0,
+    const_step_size: 1.0,
+    input_register: 1,
+    output_register: 0,
+    initial_var_value: 1.0
+};
 
 fn benchmark_small_program_creation(c: &mut Criterion) {
     c.bench_function("create_small_program", |b| {
         b.iter(|| {
-            Program::new(10)
+            Program::new(10, &CONFIG)
         })
     });
 }
@@ -15,7 +25,7 @@ fn benchmark_small_program_creation(c: &mut Criterion) {
 fn benchmark_medium_program_creation(c: &mut Criterion) {
     c.bench_function("create_medium_program", |b| {
         b.iter(|| {
-            Program::new(1000)
+            Program::new(1000, &CONFIG)
         })
     });
 }
@@ -24,13 +34,13 @@ fn benchmark_medium_program_creation(c: &mut Criterion) {
 fn benchmark_large_program_creation(c: &mut Criterion) {
     c.bench_function("create_large_program", |b| {
         b.iter(|| {
-            Program::new(10000)
+            Program::new(10000, &CONFIG)
         })
     });
 }
 
 fn benchmark_small_program_run(c: &mut Criterion) {
-    let mut p = Program::new(10);
+    let mut p = Program::new(10, &CONFIG);
     c.bench_function("run_small_program", |b| {
         b.iter(|| {
             p.run(2.0)
@@ -40,7 +50,7 @@ fn benchmark_small_program_run(c: &mut Criterion) {
 
 
 fn benchmark_medium_program_run(c: &mut Criterion) {
-    let mut p = Program::new(1000);
+    let mut p = Program::new(1000, &CONFIG);
     c.bench_function("run_medium_program", |b| {
         b.iter(|| {
             p.run(2.0)
@@ -50,7 +60,7 @@ fn benchmark_medium_program_run(c: &mut Criterion) {
 
 
 fn benchmark_large_program_run(c: &mut Criterion) {
-    let mut p = Program::new(10000);
+    let mut p = Program::new(10000, &CONFIG);
     c.bench_function("run_large_program", |b| {
         b.iter(|| {
             p.run(2.0)
