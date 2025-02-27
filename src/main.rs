cd36ebc61cff2d332c61a7bc089d94ecb4038aae
Quin-Darcy <pohmsuindraguli@gmail.com>
2025-02-26 20:48:19 -0600

    Update to use default configs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use lgp::program::{ProgramConfig};
 use lgp::population::{Population, PopulationConfig};
 
 fn main() {
@@ -24,33 +23,6 @@ fn main() {
         (-6.0, 36.0)
     ];
 
-    let prog_config = ProgramConfig {
-        total_var_registers: 8,
-        total_const_registers: 50,
-        const_start: -10.0,
-        const_step_size: 0.5,
-        input_register: 1,
-        output_register: 0,
-        initial_var_value: 0.0,
-        max_seg_len: 5,
-        max_cp_dist: 20,
-        max_seg_diff: 1,
-        mutation_step_size: 1,
-        min_prog_len: 2,
-        max_prog_len: 200
-    };
-
-    let pop_config = PopulationConfig {
-        population_size: 1000,
-        max_init_prog_size: 20,
-        crossover_rate: 0.78,
-        sa_step_size: 0.1,
-        learning_rate: 0.10,
-        reproduction_rate: 0.69,
-        tournament_size: 4,
-        prog_config
-    };
-   
-
-    let mut pop = Population::new(training_data, validation_data, pop_config);
+    let pop_config = PopulationConfig::default();
+    let pop = Population::new(training_data, validation_data, pop_config);
 }
